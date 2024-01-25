4560519b3f22cdc0f946f38231bd913b3d323438
Felipe <felipe@superhuit.ch>
2024-01-25 14:22:03 +0100

    Add Priority in Queue

--- include/RevalidateQueue.php
+++ include/RevalidateQueue.php
@@ -30,8 +30,9 @@ class RevalidateQueue {
 		$charset_collate = $wpdb->get_charset_collate();
 
 		$sql = "CREATE TABLE `{$this->table_name}` (
-			id           bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-			permalink    text                NOT NULL,
+			id         bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+			permalink  text                NOT NULL,
+			priority   int(10)             NOT NULL DEFAULT 10,
 			UNIQUE KEY permalink (permalink),
 			PRIMARY KEY  (id)
 		) $charset_collate;";
@@ -55,24 +56,40 @@ class RevalidateQueue {
 	 * Add a item to the queue
 	 *
 	 * @param string $permalink
-	 * @return void
+	 * @param int    $priority   Optional. Used to specify the order in which
+	 *                           the url are purged. Lower numbers correspond
+	 *                           with earlier purge, and urls with the same
+	 *                           priority are executed in the order in which
+	 *                           they were added. Default 10.
+	 *
+	 * @return bool Wether the permalink was added to the queue
 	 */
-	public function add_item($permalink) {
+	public function add_item( $permalink, $priority = 10 ) {
 		global $wpdb;
 
+		$inserted = false;
+
 		$wpdb->query("START TRANSACTION");
-		$in_db = $wpdb->get_results("SELECT * FROM `$this->table_name` WHERE permalink='$permalink'");
+		$in_db = $wpdb->get_results("SELECT * FROM `$this->table_name` WHERE `permalink`='$permalink'");
 		if (count($in_db) === 0) {
-			$wpdb->insert(
+			$inserted = $wpdb->insert(
 				$this->table_name,
-				[	'permalink' => $permalink ]
+				[
+					'permalink' => $permalink,
+					'priority'  => $priority
+				]
 			);
+		}
+		else {
+			$inserted = true;
 		}
 
 		$wpdb->query("COMMIT");
 
 		$njr = NextJsRevalidate::init();
 		$njr->revalidate->schedule_next_cron();
+
+		return $inserted;
 	}
 
 	/**
@@ -84,7 +101,7 @@ class RevalidateQueue {
 		global $wpdb;
 		$wpdb->query("START TRANSACTION");
 
-		$row = $wpdb->get_row("SELECT * FROM `$this->table_name` ORDER BY id ASC LIMIT 1 FOR UPDATE");
+		$row = $wpdb->get_row("SELECT * FROM `$this->table_name` ORDER BY `priority` ASC, `id` ASC LIMIT 1 FOR UPDATE");
 
 		if ($row) {
 			$wpdb->delete($this->table_name, ['id' => $row->id]);
@@ -102,6 +119,6 @@ class RevalidateQueue {
 	 */
 	public function get_queue() {
 		global $wpdb;
-		return $wpdb->get_results("SELECT * FROM `$this->table_name`");
+		return $wpdb->get_results("SELECT * FROM `$this->table_name` ORDER BY `priority` ASC, `id` ASC");
 	}
 }
